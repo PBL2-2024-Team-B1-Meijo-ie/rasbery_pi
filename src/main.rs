423c31845b01bc5387049950ac226d6175c90cdd
neruneruna7 <neruneruna7@gmail.com>
2024-11-19 15:29:17 +0900

    add: 適当URLアクセス

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
+    // 適当なAPIにアクセスしてみる
+    let url = "https://jsonplaceholder.typicode.com/todos";
+    let body = ureq::get(&url).call().unwrap().into_string().unwrap();
+    println!("{}", body);
 }
