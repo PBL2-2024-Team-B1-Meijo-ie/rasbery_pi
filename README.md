0066bfa0d384c2a3309a3a24fe85783994b94869
neruneruna7 <neruneruna7@gmail.com>
2024-11-20 13:47:16 +0900

    update: とりあえずtcp接続できる

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 ```
 armv7-unknown-linux-gnueabihf
 ```
+
+tcpの接続確認
+適当なTCPサーバーを立てる
+```bash
+nc -l 12345 < test.txt
+```
+
+プログラムを動かす
+```bash
+cargo run
+```
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
+use std::{error, io::Read, net::TcpStream};
+
 fn main() {
     println!("Hello, world!");
     // 適当なAPIにアクセスしてみる
     let url = "https://jsonplaceholder.typicode.com/todos";
     let body = ureq::get(&url).call().unwrap().into_string().unwrap();
     println!("{}", body);
+    println!("Hello, world!");
+
+    gpsd().unwrap();
+}
+
+fn gpsd() -> Result<(), Box<dyn error::Error>> {
+    let gpd_addr = "127.0.0.1:2947";
+
+    let mut stream = TcpStream::connect(gpd_addr)?;
+    println!("Connected to the server!");
+    let mut buff = [0; 10];
+    stream.read(&mut buff)?;
+    let str = buff.iter().map(|&c| c as char).collect::<String>();
+
+    println!("{:?}", str);
+    Ok(())
 }
