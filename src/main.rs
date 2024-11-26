75ddb1f8d50193edfe6868bb90de7e37ad216443
neruneruna7 <neruneruna7@gmail.com>
2024-11-26 14:11:26 +0900

    gpsから読み込みテスト

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,43 @@
-use std::{error, io::Read, net::TcpStream};
+use core::str;
+use std::{
+    error,
+    io::{self, BufRead, Read, Write},
+    net::TcpStream,
+    vec,
+};
+
+use chrono::{DateTime, Local};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize)]
+struct Query {
+    class: String,
+    enable: bool,
+    json: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize, Default)]
+#[serde(default)]
+struct TPV {
+    class: Option<String>,
+    device: Option<String>,
+    status: Option<u8>,
+    mode: Option<u8>,
+    #[serde(alias = "time")]
+    timestamp: Option<DateTime<Local>>,
+    lat: Option<f64>,
+    lon: Option<f64>,
+    alt: Option<f64>,
+    climb: Option<f64>,
+    epc: Option<f64>,
+    eps: Option<f64>,
+    ept: Option<f64>,
+    epx: Option<f64>,
+    epy: Option<f64>,
+    epv: Option<f64>,
+    track: Option<f64>,
+    speed: Option<f64>,
+}
 
 fn main() {
     // println!("Hello, world!");
@@ -16,10 +55,22 @@ fn gpsd() -> Result<(), Box<dyn error::Error>> {
 
     let mut stream = TcpStream::connect(gpd_addr)?;
     println!("Connected to the server!");
-    let mut buff = [0; 100];
-    stream.read(&mut buff)?;
-    let str = buff.iter().map(|&c| c as char).collect::<String>();
 
-    println!("{:?}", str);
+    let request_query = Query {
+        class: "WATCH".to_string(),
+        enable: true,
+        json: true,
+    };
+
+    let query = format!("?WATCH={}", serde_json::to_string(&request_query)?);
+    stream.write_all(query.as_bytes())?;
+
+    let mut reader = io::BufReader::new(&stream);
+    let mut buf = vec![];
+    loop {
+        reader.read_until(b'\n', &mut buf)?;
+        let deserialized: TPV = serde_json::from_str(str::from_utf8(&buf)?)?;
+        println!("{:?}", deserialized);
+    }
     Ok(())
 }
